1bd4f3b5df28fc64e12cef0b21773a15d193025f
Temych67 <47718914+Temych67@users.noreply.github.com>
2020-08-21 19:49:22 +0300

    Update settings.py

--- mainFolder/settings.py
+++ mainFolder/settings.py
@@ -214,6 +214,6 @@ AWS_S3_FILE_OVERWRITE = True
 AWS_DEFAULT_ACL = None
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 django_heroku.settings(locals())
