e09e079d3ae75b3ca3791599ae4e28064eec2cea
Temych67 <47718914+Temych67@users.noreply.github.com>
2020-08-28 12:24:12 +0300

    Update settings.py

--- mainFolder/settings.py
+++ mainFolder/settings.py
@@ -1,5 +1,5 @@
 import os
-
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -254,4 +254,4 @@ DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 
-
+django_heroku.settings(config=locals(), staticfiles=False,logging=False)
